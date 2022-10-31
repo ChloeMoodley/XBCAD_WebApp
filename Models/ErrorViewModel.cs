bfc7edb31e951dd1b2f63fc0095e7e0484f85064
ChloeMoodley <ST10119385@vcconnect.edu.za>
2022-10-31 14:56:47 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XBCAD_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
