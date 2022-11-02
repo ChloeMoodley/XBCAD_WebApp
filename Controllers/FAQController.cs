6fd67528c92bd4683b3f8b41a5b9944b38a233e6
Tristan McDonald <tristan.cole.mcdonald@gmail.com>
2022-11-02 15:29:36 +0200

    Added functionality for FAQ page (Model, Controller, & Index View).

---
+++ Controllers/FAQController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XBCAD_WebApp.Models;
+
+namespace XBCAD_WebApp.Controllers
+{
+    public class FAQController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Get All Employees
+            List<FAQModel> FAQList = new List<FAQModel>();
+            FAQList.Add(new FAQModel("How do I go about getting a contract?", "Contact us."));
+            return View(FAQList);
+        }
+
+
+
+    }
+}
---
+++ Models/FAQModel.cs
@@ -0,0 +1,21 @@
+﻿namespace XBCAD_WebApp.Models
+{
+    public class FAQModel
+    {
+        public int id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        public FAQModel()
+        {
+
+        }
+
+        public FAQModel(string question, string answer)
+        {
+            this.Question = question;
+            this.Answer = answer;
+        }
+
+    }
+}
