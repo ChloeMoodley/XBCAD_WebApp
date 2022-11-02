16378915ff62c7c6a7e9f997eaab3285cbde8c54
Dewald-10119314 <st10119314@vcconnect.edu.za>
2022-11-02 21:56:00 +0200

    Basic test Firebase write fully functional

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using XBCAD_WebApp.Models;
+using FireSharp.Config;
+using FireSharp.Response;
+using FireSharp.Interfaces;
+using FireSharp.Extensions;
+using FireSharp;
 
 namespace XBCAD_WebApp.Controllers
 {
     public class HomeController : Controller
     {
+
+        
+        //Init of config, used to configure FireBase client with the path
+        IFirebaseConfig ifc = new FirebaseConfig()
+        {
+            //AuthSecret = "2yHHLIYJd7mITvNBUV7cq3HVc9ItUv4nkmABbI4m",
+            BasePath = "https://skytell-fbdb-default-rtdb.europe-west1.firebasedatabase.app/"
+        };
+        IFirebaseClient fbClient;
+        
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -15,6 +30,23 @@ namespace XBCAD_WebApp.Controllers
 
         public IActionResult Index()
         {
+            try
+            {
+                //creates new DB reference (fbClient), adding the desired DB Node to the basepath of the reference config
+                IFirebaseConfig testCfg = ifc;
+                testCfg.BasePath += "Placeholder-BasicWriteTest";
+                fbClient = new FirebaseClient(testCfg);
+
+                //Writes to DB; first param = path of write; second param = data written, in this case a FAQModel object
+                fbClient.SetAsync("Test2", new FAQModel("Does this work?", "Maybe?"));
+
+            }
+            catch (Exception e)
+            {
+                
+                
+            }
+
             return View();
         }
 
