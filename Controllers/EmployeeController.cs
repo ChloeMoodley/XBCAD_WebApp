96fdce99070aa4ec0bd9c89708178b9a8b5f7263
ST10119495/Dondolo Kazembe <ST10119495@vcconnect.edu.za>
2022-11-09 21:57:52 +0200

    Creation of Employee Controller, View and Model. Also tested website

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XBCAD_WebApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Login view ()
+        public IActionResult Employee_Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,19 @@
+﻿namespace XBCAD_WebApp.Models
+{
+    public class EmployeeModel
+    {
+        // Declaration of property ()
+        public string Employee_Username { get; set; }
+        public string Employee_Password { get; set; }
+        public string Employee_Email { get; set; }
+
+        // Creation of constructor ()
+        public EmployeeModel(string employee_Username, string employee_Password, string employee_Email)
+        {
+            Employee_Username = employee_Username;
+            Employee_Password = employee_Password;
+            Employee_Email = employee_Email;
+        }
+
+    }
+}
