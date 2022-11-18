bcc2ea885b304ac7b843f5d4c3848ffaeca3635c
ST10119495/Dondolo Kazembe <ST10119495@vcconnect.edu.za>
2022-11-18 21:28:31 +0200

    Fixed Employee Controller

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -6,35 +6,17 @@ namespace XBCAD_WebApp.Controllers
 {
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        // Login view ()
 
         //create Login
-        [HttpGet]       //sends and recieve data between the client and server using the web app
-        public IActionResult Employee_Login()       //makes the html code visible on the web pages as it returns the view
+        [HttpGet] //sends and recieve data between the client and server using the web app
+        public IActionResult Employee_Login()  //makes the html code visible on the web pages as it returns the view
         {
             return View();
         }
 
-        /*        [HttpPost]  //this pushes the below method, so it can override any other method
-                [ValidateAntiForgeryToken]      //provides vailadation for any unsafe HTTP
-                public IActionResult Employee_Login([Bind] EmployeeModel objReg)
-                {
-        *//*            if (ModelState.IsValid)
-                    {
-                        return RedirectToAction("Create", "Login", new { LoginController = "" });
-                    }
 
-                    return View();*//*
-                }*/
-
-        //create register
-        [HttpGet]       //sends and recieve data between the client and server using the web app
-        public IActionResult Employee_HomePage()       //makes the html code visible on the web pages as it returns the view
+        [HttpGet] //sends and recieve data between the client and server using the web app
+        public IActionResult Employee_HomePage() //makes the html code visible on the web pages as it returns the view
         {
             return View();
         }
