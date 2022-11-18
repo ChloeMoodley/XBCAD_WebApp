8eacd3e04e0b1665d23a0a845b1106e858d7b918
ChloeMoodley <ST10119385@vcconnect.edu.za>
2022-11-18 12:54:49 +0200

    Made about us view and controller

---
+++ Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XBCAD_WebApp.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
