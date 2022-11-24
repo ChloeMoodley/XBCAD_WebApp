04045632cf24acbfa5d7e75ef2670b02ccac2775
ChloeMoodley <ST10119385@vcconnect.edu.za>
2022-11-24 12:17:20 +0200

    Attempted to implement authenication

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,4 +1,7 @@
-﻿using FireSharp.Config;
+﻿using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Database.Query;
+using FireSharp.Config;
 using FireSharp.Interfaces;
 using FireSharp.Response;
 using Microsoft.AspNetCore.Mvc;
@@ -12,14 +15,21 @@ namespace XBCAD_WebApp.Controllers
     public class EmployeeController : Controller
     {
   
-        //Init of config, used to configure FireBase client with the path
+/*        //Init of config, used to configure FireBase client with the path
         //Referencing: https://www.freecodespot.com/blog/dotnet-core-crud-with-firebase-database/
         IFirebaseConfig ifc = new FirebaseConfig()
         {
             //AuthSecret = "2yHHLIYJd7mITvNBUV7cq3HVc9ItUv4nkmABbI4m",
             BasePath = "https://skytell-fbdb-default-rtdb.europe-west1.firebasedatabase.app/"
         };
-        IFirebaseClient fbClient;
+        IFirebaseClient fbClient;*/
+
+        FirebaseAuthProvider auth;
+        public EmployeeController()
+        {
+            auth = new FirebaseAuthProvider(
+                            new Firebase.Auth.FirebaseConfig("AIzaSyBQgTUBNFnfsSRRU2er5kRpVxzIeGBCxlI"));
+        }
 
         //create Login
         [HttpGet] //sends and recieve data between the client and server using the web app
@@ -29,17 +39,32 @@ namespace XBCAD_WebApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult Employee_Login([Bind] EmployeeModel EmpObj)
+        public async Task<IActionResult> Employee_Login(EmployeeModel EmpObj)
         {
-            fbClient = new FireSharp.FirebaseClient(ifc);
-            FirebaseResponse response = fbClient.Get("Users");
+
+                //log in the user
+                var fbAuthLink = await auth.SignInWithEmailAndPasswordAsync(EmpObj.emp_Email, EmpObj.emp_Password);
+                string token = fbAuthLink.FirebaseToken;
+                //saving the token in a session variable
+                if (token != null)
+                {
+                    HttpContext.Session.SetString("_UserToken", token);
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View();
+                }
+            /*fbClient = new FireSharp.FirebaseClient(ifc);
+            FirebaseResponse response = fbClient.Get("Users/");
             dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
 
             if (data != null)
             {
                 foreach (var item in data)
                 {
-                    if (EmpObj.Employee_Email.Equals(item))
+                    if (EmpObj.emp_Email.Equals(item))
                     {
                         return RedirectToAction("Employee_HomePage");
                     }
@@ -48,9 +73,7 @@ namespace XBCAD_WebApp.Controllers
                         return RedirectToAction("Employee_Login");
                     }
                 }
-            }
-
-            return View();
+            }*/
         }
 
 
@@ -59,6 +82,40 @@ namespace XBCAD_WebApp.Controllers
         {
             return View();
         }
+
+       /* public async Task<ActionResult> Employee_HomePage()
+        {
+            //Simulate test user data and login timestamp
+            var userId = "12345";
+            var currentLoginTime = DateTime.UtcNow.ToString("MM/dd/yyyy HH:mm:ss");
+
+            //Save non identifying data to Firebase
+            var currentUserLogin = new LoginViewModel() { TimestampUtc = currentLoginTime };
+            var firebaseClient = new FirebaseClient("yourFirebaseProjectUrl");
+            var result = await firebaseClient
+              .Child("Users/" + userId + "/Logins")
+              .PostAsync(currentUserLogin);
+
+            //Retrieve data from Firebase
+            var dbLogins = await firebaseClient
+              .Child("Users")
+              .Child(userId)
+              .Child("Logins")
+              .OnceAsync<LoginViewModel>();
+
+            var timestampList = new List<DateTime>();
+
+            //Convert JSON data to original datatype
+            foreach (var login in dbLogins)
+            {
+                timestampList.Add(Convert.ToDateTime(login.Object.TimestampUtc).ToLocalTime());
+            }
+
+            //Pass data to the view
+            ViewBag.CurrentUser = userId;
+            ViewBag.Logins = timestampList.OrderByDescending(x => x);
+            return View();
+        }*/
 
     }
 }
--- Models/EmployeeModel.cs
+++ Models/EmployeeModel.cs
@@ -1,16 +1,40 @@
-﻿namespace XBCAD_WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace XBCAD_WebApp.Models
 {
     public class EmployeeModel
     {
         // Declaration of property ()
-        public string Employee_Password { get; set; }
-        public string Employee_Email { get; set; }
+
+        [Key]
+        public int emp_id { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string emp_Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string emp_Password { get; set; }
+
+
+        /*        public string Employee_Password { get; set; }
+                public string Employee_Email { get; set; }*/
 
         // Creation of constructor ()
+
+        public EmployeeModel()
+        {
+            
+        }
+
         public EmployeeModel(string employee_Password, string employee_Email)
         {
-            Employee_Password = employee_Password;
-            Employee_Email = employee_Email;
+            employee_Password = employee_Password;
+            employee_Email = employee_Email;
         }
 
     }
